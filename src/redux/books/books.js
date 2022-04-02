6bd52649f09fee42ad451353c915eadfd0f8b229
Joshua O. Ivie <joshua@lifeonline.com.ng>
2022-04-03 00:01:15 +0100

    Add Book Reducer and Actions

---
+++ src/redux/books/books.js
@@ -0,0 +1,35 @@
+// Action Types
+const ADD_BOOK = 'ADD_BOOK';
+const REMOVE_BOOK = 'REMOVE_BOOK';
+
+// Initial State
+const initialState = [];
+
+// Action Creators
+export const addBook = ({ id, author, title }) => ({
+  type: ADD_BOOK,
+  payload: {
+    id,
+    author,
+    title,
+  },
+});
+
+export const removeBook = (id) => ({
+  type: REMOVE_BOOK,
+  payload: id,
+});
+
+// Reducers
+const bookReducer = (state = initialState, action = {}) => {
+  switch (action.type) {
+    case ADD_BOOK:
+      return [...state, action.payload];
+    case REMOVE_BOOK:
+      return state.filter((book) => book.id !== action.payload);
+    default:
+      return state;
+  }
+};
+
+export default bookReducer;
