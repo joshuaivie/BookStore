9093e12424cb288de758b3256255cc96a12205a2
Joshua Ivie <joshua@lifeonline.com.ng>
2022-04-03 03:04:07 +0100

    Remove Commented Code

--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -2,40 +2,9 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import BookList from '../components/Books/BookList';
 import BookForm from '../components/Books/BookForm';
-// import LoadingScreen from '../components/Shared/LoadingScreen';
 
 function Books() {
-  // const [loadingBookData, setLoadingBookData] = useState(true);
-  // const [bookData, setBookData] = useState([]);
-  // const [categories, setCategories] = useState([]);
-
   const books = useSelector((state) => state.books);
-  console.log(books);
-  // useEffect(() => {
-  //   setLoadingBookData(true);
-  //   fetch('../data/books.json', {
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //       Accept: 'application/json',
-  //     },
-  //   })
-  //     .then((response) => {
-  //       if (response.ok) {
-  //         return response.json();
-  //       }
-  //       throw response;
-  //     })
-  //     .then((data) => {
-  //       setBookData(data);
-  //       setCategories([]);
-  //     })
-  //     .catch((error) => {
-  //       console.log('Error loading data', error);
-  //     })
-  //     .finally(() => {
-  //       setLoadingBookData(false);
-  //     });
-  // }, []);
 
   return (
     <div>
