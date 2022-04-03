9e81dda9df2cc883d92653b7ee0b0f12e0735f52
Joshua O. Ivie <joshua@lifeonline.com.ng>
2022-04-03 02:01:39 +0100

    Add functtion for Adding new book

--- src/components/Books/BookForm.jsx
+++ src/components/Books/BookForm.jsx
@@ -1,14 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { v4 as uuidv4 } from 'uuid';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../../redux/books/books';
 
 function BookForm({ categories }) {
+  const dispatch = useDispatch();
+
+  const [formState, setFormState] = useState({});
+
+  const resetForm = (e) => {
+    const form = e.target;
+    Array.from(form.querySelectorAll('input')).forEach(
+      // eslint-disable-next-line no-param-reassign
+      (input) => { input.value = ''; },
+    );
+    setFormState({});
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (formState.title && formState.author) {
+      const finalFormState = { ...formState };
+      finalFormState.id = uuidv4();
+      dispatch(addBook(finalFormState));
+      resetForm(e);
+    }
+  };
+
+  const handleChange = (e) => {
+    const { value, name } = e.target;
+    const updatedFormState = { ...formState };
+
+    updatedFormState[name] = value;
+    setFormState(updatedFormState);
+  };
+
   return (
     <div>
       <div className="section-title">Add New Book</div>
-      <form action="" method="post">
+      <form onSubmit={handleSubmit}>
         <ul>
           <li>
-            <input type="text" placeholder="Book Title" required />
+            <input type="text" name="title" placeholder="Book Title" onChange={handleChange} required />
+          </li>
+          <li>
+            <input type="text" name="author" placeholder="Book Author" onChange={handleChange} required />
           </li>
           <li>
             <select name="categories" id="select-category">
